96189b03b7a9e4e9c26630eadebc6aea79b033b8
Junichi Ito <jit@sonicgarden.jp>
2016-02-28 10:36:57 +0900

    Add answer for question 2

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,3 +1,9 @@
 class Discussion < ActiveRecord::Base
   has_many :comments
+
+  scope :with_comment_counts, -> {
+    select("discussions.id, MAX(discussions.content) AS content, COUNT(c.id) AS comment_count")
+      .joins("LEFT OUTER JOIN comments c ON c.discussion_id = discussions.id")
+      .group(:id)
+  }
 end
