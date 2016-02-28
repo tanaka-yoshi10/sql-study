ec8dbc97a28bf61cfb46c52518721bdbf1a84dcb
Junichi Ito <jit@sonicgarden.jp>
2016-02-29 08:15:56 +0900

    Refactoring

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,9 +1,4 @@
 class Member < ActiveRecord::Base
   has_many :event_participations
   has_many :events, through: :event_participations
-
-  scope :no_participation, ->(event) {
-    member_scope = event.members.where(event_participations: {canceled: false}).select(:id)
-    where.not(id: member_scope)
-  }
 end
