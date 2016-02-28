350b65178583d413fe2d76cab4fda0cc10f57135
Junichi Ito <jit@sonicgarden.jp>
2016-02-28 13:06:54 +0900

    Add spec for question 3

---
+++ spec/features/question_3_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+feature 'Question 2' do
+  scenario do
+    visit q3_questions_path
+
+    rows = all('.answer tr')
+    within rows[1] do
+      expect(page).to have_content '山登り'
+      expect(page).to have_content '予算を決める'
+      expect(page).to have_content '2026-02-01'
+    end
+    within rows[2] do
+      expect(page).to have_content '新人歓迎会'
+      expect(page).to have_content '参加者を募る'
+      expect(page).to have_content '2026-03-01'
+    end
+    within rows[3] do
+      expect(page).to have_content 'Elixir勉強会'
+    end
+  end
+end
