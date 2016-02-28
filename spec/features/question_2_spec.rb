975a50b0118f9ff73ccb9df0e9e35494af14b19c
Junichi Ito <jit@sonicgarden.jp>
2016-02-28 10:15:59 +0900

    Add spec for question 2

---
+++ spec/features/question_2_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature 'Question 2' do
+  scenario do
+    visit q2_questions_path
+
+    rows = all('.answer tr')
+    within rows[1] do
+      expect(page).to have_content 'Ruby'
+      expect(page).to have_content '2件'
+    end
+    within rows[2] do
+      expect(page).to have_content 'Java'
+      expect(page).to have_content '1件'
+    end
+    within rows[3] do
+      expect(page).to have_content 'COBOL'
+      expect(page).to have_content '0件'
+    end
+  end
+end
