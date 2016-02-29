99d1e8a70d0e06e0b9364b3d209da3d7137e7028
Junichi Ito <jit@sonicgarden.jp>
2016-03-01 06:37:56 +0900

    Refactoring

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,7 +1,7 @@
 class Discussion < ActiveRecord::Base
   has_many :comments
 
-  scope :with_comment_counts, -> {
+  scope :order_by_comment_counts, -> {
     select("discussions.id, MAX(discussions.content) AS content, COUNT(c.id) AS comment_count")
       .joins("LEFT OUTER JOIN comments c ON c.discussion_id = discussions.id")
       .order("comment_count DESC")
