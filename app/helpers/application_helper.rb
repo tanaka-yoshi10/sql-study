28070b80236f4b69dc1df033360d770be4b04b1e
Junichi Ito <jit@sonicgarden.jp>
2016-03-01 05:26:07 +0900

    Format SQL

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
+require "anbt-sql-formatter/formatter"
+
 module ApplicationHelper
+  def format_sql(sql)
+    rule = AnbtSql::Rule.new
+    formatter = AnbtSql::Formatter.new(rule)
+    formatter.format(sql)
+  end
 end
