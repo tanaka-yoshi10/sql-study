95248ca1ed2e02a8440bebd21e6a10cae32542b4
Junichi Ito <jit@sonicgarden.jp>
2016-03-02 06:22:42 +0900

    Tweak count style

--- spec/features/question_2_spec.rb
+++ spec/features/question_2_spec.rb
@@ -7,15 +7,15 @@ feature 'Question 2' do
     rows = all('.answer tr')
     within rows[1] do
       expect(page).to have_content 'Ruby'
-      expect(page).to have_content '2件'
+      expect(page).to have_content '2'
     end
     within rows[2] do
       expect(page).to have_content 'Java'
-      expect(page).to have_content '1件'
+      expect(page).to have_content '1'
     end
     within rows[3] do
       expect(page).to have_content 'COBOL'
-      expect(page).to have_content '0件'
+      expect(page).to have_content '0'
     end
     assert_sql_count
   end
